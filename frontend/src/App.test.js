11ba49738e2502665463a7e8e10183783e9fb491
David Amebley <davameb@gmail.com>
2024-02-21 00:31:47 +0200

    Update test script to check for the presence of the connect button instead

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('checks for the presence of the connect button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const connectButton = screen.getByText(/Connect/i);
+  expect(connectButton).toBeInTheDocument();
 });
